41451fdad4f0fe96a92fdc7f04f659be310e37f9
Usman Tahir <Usman.tahir@synergy-it.com>
2025-01-20 22:10:28 +0500

    #19, Test Application

---
+++ ConferenceAttendees.API/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConferenceAttendees.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController(IConfiguration configuration) : ControllerBase
+    {
+        private readonly IConfiguration _configuration = configuration;
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            return _configuration["EnvironmentValue"];
+        }
+    }
+}
--- ConferenceAttendees.API/Program.cs
+++ ConferenceAttendees.API/Program.cs
@@ -1,20 +1,32 @@
+using ConferenceAttendees.Api.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ConferenceAttendeeDatabaseConnection"));
+});
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var dbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    dbContext.Database.Migrate();
 }
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
 app.UseHttpsRedirection();
 
